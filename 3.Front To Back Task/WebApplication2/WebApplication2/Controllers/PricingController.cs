df85e98a02d2869838b21d7017609aab0cb5e658
IlahaLatifova <7582ety@code.edu.az>
2022-12-30 01:59:21 +0400

    PurpleBuzz

---
+++ 3.Front To Back Task/WebApplication2/WebApplication2/Controllers/PricingController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication2.DAL;
+using WebApplication2.ViewModel;
+
+namespace WebApplication2.Controllers
+{
+    public class PricingController : Controller
+    {
+        private readonly AppDbContext _context;
+        public PricingController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            WorkVM workVM = new WorkVM()
+            {
+                Categories = await _context.Categories.ToListAsync(),
+                Products = await _context.Products
+                .Include(p => p.Category)
+                .ToListAsync()
+            };
+            return View(workVM);
+        }
+
+    }
+}
